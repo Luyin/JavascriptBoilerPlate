16446b3f830f2adef7a7ee173b8ae2b5c859c1a9
Luyin-PC\Luyin <myraous@gmail.com>
2018-01-05 07:58:14 +0900

    chore: support typescript

--- config/webpack/webpack.common.js
+++ config/webpack/webpack.common.js
@@ -9,22 +9,30 @@ const path = require('path');
 
 
 module.exports = {
-  entry: {
-      app: './src/index.js'
-  },
-  output : {
-      path: path.resolve(__dirname, '../../dist'),
-      filename: '[name].bundle.js'
-  },
-  module : {
-      rules : [
-          {
-              test: /\.css$/,
-              use: [
-                  'style-loader',
-                  'css-loader'
-              ]
-          }
-      ]
-  }
+    entry: {
+        app: './src/index.ts'
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
+    output: {
+        path: path.resolve(__dirname, '../../dist'),
+        filename: '[name].bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    }
 };

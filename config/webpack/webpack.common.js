eef2a1d7da878592b76723aff911a83c117d8bdd
Luyin-PC\Luyin <myraous@gmail.com>
2018-02-06 01:04:30 +0900

    chore: Added sass extracted mode setting

--- config/webpack/webpack.common.js
+++ config/webpack/webpack.common.js
@@ -1,3 +1,4 @@
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const path = require('path');
 
 /**
@@ -10,7 +11,7 @@ const path = require('path');
 
 module.exports = {
     entry: {
-        app: './src/index.ts'
+        main: ['./src/index.ts','./src/assets/sass/main.scss']
     },
     resolve: {
         extensions: ['.tsx', '.ts', '.js']
@@ -21,12 +22,36 @@ module.exports = {
     },
     module: {
         rules: [
+            /**
+             *  JS Bundle 모듈 내에 inline 모드
+             */
+            // {
+            //     test: /\.scss$/,
+            //     use: [
+            //         'style-loader', // Javascript String을 Style node로 변환하여 생성
+            //         'css-loader',   // CSS를 CommonJS로 불러들일 수있도록 Javascript String 형태로 변형
+            //         'sass-loader'   // Sass 를 컴파일하여 CSS 변환
+            //     ]
+            // },
+            /**
+             *   CSS 파일 분리 모드
+             */
             {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader'
-                ]
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: [{
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    }, {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    }]
+                })
             },
             {
                 test: /\.tsx?$/,
@@ -34,5 +59,10 @@ module.exports = {
                 exclude: /node_modules/
             }
         ]
-    }
+    },
+    plugins: [
+        new ExtractTextPlugin({
+            filename: 'style/[name]-one.css' //output.path 경로 아래에 생성된다.
+        })
+    ]
 };

028cc134a511bb6d3d10e026d5cb2e561ae8988b
Luyin-PC\Luyin <myraous@gmail.com>
2018-03-04 20:44:30 +0900

    feat: updated sample

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,6 @@
-import * as _ from 'lodash';
-import './assets/style/index.css';
-
 function component() {
     var element = document.createElement('div');
 
-    element.innerHTML = _.join(['안녕', 'webpack'], ' ');
     element.classList.add('hello');
 
     return element;
